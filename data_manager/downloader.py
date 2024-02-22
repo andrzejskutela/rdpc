e5b35051d5b9dbbe176733dec3aca3be2cd7fd9c
PenguinHerder <skutelaandrzej@yahoo.com>
2024-02-22 14:49:33 +0000

    file downloader

--- data_manager/downloader.py
+++ data_manager/downloader.py
@@ -1,5 +1,6 @@
 import os
 import re
+import requests
 from .datamodule import DataModule
 
 class Downloader(DataModule):
@@ -9,14 +10,26 @@ class Downloader(DataModule):
 		self.working_dir = working_dir
 		super().registerAllowedMethods(['get'])
 
-	def get(self, project: str, download: str):
+	def get(self, project: str, download: str, save_as: str):
 		if not self.project_is_valid(project):
 			if self.project_path_is_valid(project):
 				self.create_empty_project(project)
 			else:
 				raise Exception(f"Path ")
 
-		self.add_file_to_project(project, download)
+		self.add_file_to_project(project, download, save_as)
+
+	def download_file(self, project: str, download: str):
+		ret = {}
+		with open(self.path_join([project, "download.tmp"]), "wb") as file:
+			r = requests.get(download, stream=True)
+			if r.status_code == requests.codes.ok:
+				file.write(r.content)
+				ret['size'] = len(r.content)
+			else:
+				r.raise_for_status()
+
+		return ret
 
 	def create_empty_project(self, project: str):
 		os.makedirs(self.path_join([project, "import_files"]), exist_ok=True)
@@ -26,9 +39,14 @@ class Downloader(DataModule):
 !.gitignore
 """)
 
-	def add_file_to_project(self, project: str, download: str):
+	def add_file_to_project(self, project: str, download: str, save_as: str):
+		if not re.search("^[a-z0-9_]{3,40}\\.[a-z]{1,5}$", save_as):
+			raise Exception(f"Invalid save as value {save_as!r}")
+
+		data = self.download_file(project, download)
+		os.rename(self.path_join([project, "download.tmp"]), self.path_join([project, "import_files", save_as]))
 		with open(self.path_join([project, "import_list.txt"]), "a") as file:
-			file.write(download + "\n")
+			file.write(f"{save_as} {data['size']} {download}\n")
 
 	def project_path_is_valid(self, project: str):
 		not_allowed = ["data_manager"]
--- manager.py
+++ manager.py
@@ -1,4 +1,4 @@
-#!//usr/bin/python3.12
+#!//usr/bin/python3.11
 
 import os
 import sys
